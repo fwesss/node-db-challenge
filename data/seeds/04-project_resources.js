8ae04ae4816fd851705ac3adf88f7f2c45780ced
Westley Feller <feller.westley@gmail.com>
2020-02-09 19:10:51 -0800

    Seeds

---
+++ data/seeds/04-project_resources.js
@@ -0,0 +1,39 @@
+exports.seed = knex =>
+  knex('project_resources').insert([
+    {
+      project_id: 1,
+      resource_id: 1,
+    },
+    {
+      project_id: 1,
+      resource_id: 2,
+    },
+    {
+      project_id: 1,
+      resource_id: 3,
+    },
+    {
+      project_id: 2,
+      resource_id: 1,
+    },
+    {
+      project_id: 2,
+      resource_id: 2,
+    },
+    {
+      project_id: 2,
+      resource_id: 3,
+    },
+    {
+      project_id: 3,
+      resource_id: 1,
+    },
+    {
+      project_id: 3,
+      resource_id: 2,
+    },
+    {
+      project_id: 3,
+      resource_id: 3,
+    },
+  ])
